28637b4464c33a55aa97bfa86163592b73c4b525
Roger Hom <roger.c.hom@gmail.com>
2015-08-02 14:21:50 -0700

    Implement launch view

--- UCD Map v2/LaunchViewController.m
+++ UCD Map v2/LaunchViewController.m
@@ -6,17 +6,23 @@
 //  Copyright (c) 2015 Logical Dimension. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LaunchViewController.h"
 
-@interface ViewController ()
+@interface LaunchViewController ()
 
 @end
 
-@implementation ViewController
+@implementation LaunchViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    double delayInSeconds = 2.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self performSegueWithIdentifier:@"start" sender:self];
+    });
 }
 
 - (void)didReceiveMemoryWarning {
