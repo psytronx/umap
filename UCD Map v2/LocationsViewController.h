2eb25c78ef05125a36987c2e6d463a263e25b123
Roger Hom <roger.c.hom@gmail.com>
2015-08-02 15:38:44 -0700

    Add UITableView and UISearchBar to LocationsViewController

---
+++ UCD Map v2/LocationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationsViewController.h
+//  UCD Map v2
+//
+//  Created by psytronx on 8/2/15.
+//  Copyright (c) 2015 Logical Dimension. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
+
+@end
---
+++ UCD Map v2/LocationsViewController.m
@@ -0,0 +1,37 @@
+//
+//  LocationsViewController.m
+//  UCD Map v2
+//
+//  Created by psytronx on 8/2/15.
+//  Copyright (c) 2015 Logical Dimension. All rights reserved.
+//
+
+#import "LocationsViewController.h"
+
+@interface LocationsViewController ()
+
+@end
+
+@implementation LocationsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
