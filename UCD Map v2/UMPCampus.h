cd586c2ef85f5f010195d664ca5858f2b0d820a3
Roger Hom <roger.c.hom@gmail.com>
2015-08-02 21:37:20 -0700

    Simplify UMPCampus model class; get rid of functionality and properties not needed for single school app

--- UCD Map v2/UMPCampus.h
+++ UCD Map v2/UMPCampus.h
@@ -1,6 +1,7 @@
 //
 //  UMPCampus.h
 //  University Maps
+//  Note: This is a striped-down verson for single-school app.
 //
 //  Created by psytronx on 8/31/14.
 //  Copyright (c) 2014 Logical Dimension. All rights reserved.
@@ -8,14 +9,8 @@
 
 #import <Foundation/Foundation.h>
 
-@interface UMPCampus : NSObject <NSCoding>
-@property (nonatomic) NSInteger id;
+@interface UMPCampus : NSObject
 @property (nonatomic, copy) NSString * campusName;
 @property (nonatomic, copy) NSString * campusCode;
-@property (nonatomic, copy) NSString * state;
-@property (nonatomic) double centerLat;
-@property (nonatomic) double centerLong;
-@property (nonatomic) double spanLat;
-@property (nonatomic) double spanLong;
 @property (nonatomic, copy) NSString * wikipediaUrl;
 @end
